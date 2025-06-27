373572b2dcc75206ed706b8ff1951e407a65045a
NiTiSon <NiTiSon@users.noreply.github.com>
2025-06-27 11:49:51 +0300

    fix publish

---
+++ NiTiS.FileSize.Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NiTiS.Test;
+
+public class SortTest
+{
+	[Test]
+	public async Task OrderTest()
+	{
+		Random random = new Random();
+		ulong[] ints = new ulong[10];
+		FileSize[] sizes = new FileSize[10];
+
+		for (int i = 0; i < 10; i++)
+		{
+			ints[i] = (ulong)random.Next();
+			sizes[i] = new FileSize(ints[i]);
+		}
+
+		Array.Sort(ints);
+		Array.Sort(sizes);
+
+		await Assert.That(ints).IsEquivalentTo(sizes.Select(t => t.Bytes));
+	}
+}
--- public/NUGET_README.md
+++
@@ -1,6 +0,0 @@
-# NiTiS.FileSize
-
-This package provides new types:
-+ NiTiS.FileSize
-+ NiTiS.FileSizeUnit
-+ NiTiS.FileSizeSystem
